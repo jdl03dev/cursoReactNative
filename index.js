13cad32cacaae39468d2d99c33d40da99374a72d
jdl03dev <jdl03dev@gmail.com>
2025-05-26 12:27:19 -0500

    solucion de compilacion, se realizaron cambios en versionamiento de module y moduleResolution a Node16 en tsconfig.json es importante iniciar metro asi que hay que tener en cuenta que se debe estar ejecutando el comando npx react-native start

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import {AppRegistry} from 'react-native';
 
 import Login from './Componentes/Login';
 import App from './Componentes/App';
+import Pruebas from './Componentes/pruebas';
 import Registro from './Componentes/Registro';
+
 import {name as appName} from './app.json';
 AppRegistry.registerComponent(appName, () => Registro);
